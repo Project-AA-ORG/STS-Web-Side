91fdf22ba7615787a0d727fdfaf6991c9c1a6a54
Furkan2001 <ahmetfurkanekinci@hotmail.com>
2023-11-16 16:46:28 +0300

    controller içinde session ile geçiçci database tutarak login işlemini güçlendirdim ve login için session.php dosyasında bazı ayarlamalar yaptım.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,13 +11,12 @@ class LoginController extends Controller
         return  view("login");
     }
 
+    // Kullanıcı doğru giriş yapmış mı yapmamış mı kontrolü yapılır.
     public function controlForLogin(Request $request){
-
         $adminInfo = Admin::getAllAdmins();
         if ($adminInfo[0]->username == $request->username){
             if($adminInfo[0]->password == $request->password){
-                // $data["successful"] = 1;
-                // return $data;
+                session(['login_control' => 1]); // login_control adında bir session tutuyorum ve kullanıcı giriş yapmış mı diye diğer tüm fonksiyonlarda kontrol yapacağım.
                 return view("deneme");
             }
         }
--- config/session.php
+++ config/session.php
@@ -31,9 +31,9 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 480), //session 480 dk sonra temizlenir
 
-    'expire_on_close' => false,
+    'expire_on_close' => true, //tarayıcı kapatıldığı an session temizlenir
 
     /*
     |--------------------------------------------------------------------------
